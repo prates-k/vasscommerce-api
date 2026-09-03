b0096f6c7e8f09ca51bf9259742ca3013af2e903
prates-k <eng.kauaprates@gmail.com>
2026-09-03 16:45:40 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ src/main/java/br/com/kauaprates/vasscommerce/VasscommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.kauaprates.vasscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasscommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kauaprates/vasscommerce/VasscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kauaprates.vasscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
